3c5eaf557fb12f2cef76ed72cf1b7915fa6be341
Jorge97118 <sdf17631@gmail.com>
2025-11-21 22:24:13 -0600

    Se agregó usuarios.sql

---
+++ usuarios.sql
@@ -0,0 +1,7 @@
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(200) NOT NULL,
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
